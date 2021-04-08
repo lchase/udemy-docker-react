6bb8b8b6bdfd648baa9de8730cfa6c2e76cf7296
Lawrence Chase <lawrence.rjw.chase@gmail.com>
2021-04-07 17:55:53 -0700

    Feat one (#2)
    * changed text
    
    * feat one

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Changed via feature branch!" link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link 2', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Changed via feature branch!/i);
   expect(linkElement).toBeInTheDocument();
 });
